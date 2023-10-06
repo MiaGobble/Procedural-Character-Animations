07ece4a0c31279b5c6918640729742e5e5df76df
Iconic Gaming <96698128+MiaGobble@users.noreply.github.com>
2023-10-06 11:47:37 -0500

    Create config.lua

---
+++ src/config.lua
@@ -0,0 +1,6 @@
+return {
+    legStride = 2, -- Stride of the legs in studs
+    legCycleSpeed = 1, -- Speed of the leg cycle
+    maxRenderDistance = 300, -- Max render distance of character animation from camera cframe
+    maxFramerate = 1 / 1000, -- Max framerate of the animation, in seconds
+}
